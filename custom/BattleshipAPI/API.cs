c0f2f432b963bb5defdc40c6c2eb720922388039
U-DESKTOP-M6P7BFH\riley <rileyduke@gmail.com>
2019-02-01 16:33:41 +0900

    add project

---
+++ custom/BattleshipAPI/API.cs
@@ -0,0 +1,70 @@
+﻿using Amazon.Lambda.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sampleFactCsharp.BattleshipAPI
+{
+    public class API
+    {
+        private ILambdaContext context { get; set; }
+
+        public API(ILambdaContext ctx)
+        {
+            this.context = ctx;
+        }
+
+        public void LogTest(string log)
+        {
+            this.Log(log);
+        }
+
+        /// <summary>
+        /// move right
+        /// </summary>
+        /// <param name="player_id"></param>
+        public void MoveRight(string player_id)
+        {
+            try
+            {
+                APIHelper.Get(APIHelper.RIGHT);
+            }
+            catch (Exception e)
+            {
+                this.Log(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// register player
+        /// </summary>
+        /// <param name="player_name"></param>
+        /// <returns></returns>
+        public string RegisterPlayer(string player_name)
+        {
+            try
+            {
+                return APIHelper.Get(String.Format(APIHelper.REGISTER, player_name));
+            }
+            catch (Exception e)
+            {
+                this.Log(e.Message);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// logger interface
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>void</returns>
+        private void Log(string text)
+        {
+            if (context != null)
+            {
+                context.Logger.LogLine(text);
+            }
+        }
+    }
+}
---
+++ custom/BattleshipAPI/APIHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Http;
+using Amazon.Lambda.Core;
+
+namespace sampleFactCsharp.BattleshipAPI
+{
+    public static class APIHelper
+    {
+        private static readonly HttpClient client = new HttpClient();
+        
+        // API URL info
+        public static string HOST = @"http://ec2-52-68-115-158.ap-northeast-1.compute.amazonaws.com";
+        public static string PORT = @"3001";
+        public static string API = @"/api/player/";
+        public static string URL = HOST + ":" + PORT + API;
+
+        // movement
+        public static string LEFT = URL + @"{0}/move/left";
+        public static string UP = URL + @"{0}/move/up";
+        public static string DOWN = URL + @"{0}/move/down";
+        public static string RIGHT = URL + @"{0}/move/right";
+
+        // register
+        public static string REGISTER = URL + @"/add/{0}";
+
+
+        
+
+        /// <summary>
+        /// simple get request. 
+        /// returns the response as a string
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public static string Get(string uri)
+        {
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                client.BaseAddress = new Uri(uri);
+                HttpResponseMessage response = client.GetAsync("answers?order=desc&sort=activity&site=stackoverflow").Result;
+                response.EnsureSuccessStatusCode();
+                string result = response.Content.ReadAsStringAsync().Result;
+                //Console.WriteLine("Result: " + result);
+
+                return result;
+            }
+        }
+    }
+}
